350e01cbbf232f4fff7cf80d5985c5987277d46b
reinhartP <reinhartjp@gmail.com>
2020-04-28 18:34:43 -0700

    modified function names

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class API:
     def __init__(
         self, client_id, client_secret, bearer_token=None, bearer_token_expiration=0
@@ -28,14 +29,9 @@ class API:
         self.__bearer_token = response["access_token"]
         self.__bearer_token_expiration = float(time.time() + response["expires_in"])
 
-    def get_bearer_token(self):
-        return self.__bearer_token
-
-    def get_bearer_token_expiration(self):
-        return self.__bearer_token_expiration
-
     def __handle_rate_limit(self):
         if self.rate_limit_remaining == 0:
+            logger.error("rate limit reached")
             time_to_sleep = min(self.rate_limit_reset - time.time(), 10)
             time_to_sleep = max(time_to_sleep, 1)
 
@@ -47,17 +43,32 @@ class API:
             self.rate_limit_remaining = int(response.headers.get("Ratelimit-Remaining"))
             self.rate_limit_reset = int(response.headers.get("Ratelimit-Reset"))
 
-    def __headers(self):
+    def __get_headers(self):
         return {
             "Authorization": f"Bearer {self.__bearer_token}",
             "Client-ID": self.__client_id,
         }
 
     def request(self, method, url, **kwargs):
+        """
+            Sends request to endpoint and returns the response
+
+            Sends the request while handling the rate limit and bearer token.
+
+            Parameters
+            ----------
+            method : str
+                HTTP method for the request
+            url : str
+                api endpoint
+            **kwargs : dict
+                other parameters for a request. typically just params
+        """
         request = requests.Request(
-            method, url, headers=self.__headers(), **kwargs,
+            method, url, headers=self.__get_headers(), **kwargs,
         ).prepare()
         self.__handle_rate_limit()
+        self.__handle_bearer_token()
         while True:
             try:
                 response = requests.Session().send(request)
@@ -74,3 +85,9 @@ class API:
                 break
 
         return response.json()
+
+    def get_bearer_token(self):
+        return self.__bearer_token
+
+    def get_bearer_token_expiration(self):
+        return self.__bearer_token_expiration
